3e63a2039bd4457d30ed7cef900dc12f3fcccb88
danielnachun <daniel.nachun@gmail.com>
2021-07-22 14:13:06 -0700

    RNASeq_hsapiens: add workflow

---
+++ RNASeq_hsapiens/workflow_config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+star_index="/labs/sjaiswal/genomes/GRCh38/STAR_genome" #path to STAR genome index
+
+annotation_gtf="/labs/sjaiswal/genomes/GRCh38/gencode.v35.annotation.gtf" #path to annotation GTF
+
+reference_genome="/labs/sjaiswal/genomes/GRCh38/GRCh38.p13.genome.fa" #location of reference genome used in alignment
+
+annotation_refFlat="/labs/sjaiswal/genomes/GRCh38/gencode.v35.annotation.fixed.refFlat.txt" ## Created from ensembl GTF using gtfToGenePred -genePredExt -geneNameAsName2 from bioconda
+
+gene_key="/labs/sjaiswal/genomes/mm10/gencode.vM20.primary_assembly.annotation.genekey.txt" #path to annotation GTF
