ca56131066b2440eee167c8a9a245b38dbfba145
maurermaggie <maurertm@stanford.edu>
2021-10-28 20:01:55 +0000

    submit_BWA_CHIP: change location of fastq_list

--- BWA_CHIP/submit_BWA_CHIP.sh
+++ BWA_CHIP/submit_BWA_CHIP.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "run_BWA_mutect [fastq_directory] [output_directory]"
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo "Format: ./submit_BWA_CHIP.sh [fastq_directory] [output_directory] --argument"
     echo "fastq_directory: path to raw .fastq or .fastq.gz files"
     echo "output_directory: path for BWA and mutect output"
+    echo "argument: indicates requested analysis technique(s) (--mutect, --varscan, --haplotypecaller, or --all)."
+    echo "If --varscan is selected, you may use the optional arguments --p_value, --min_var_freq, and --min_coverage (if so desired)." 
+    exit 1
 else
     TEMP=`getopt -o vdm: --long min_coverage:,min_var_freq:,p_value:,mutect,varscan,haplotypecaller,all \
     -n 'submit_BWA_CHIP.sh' -- "$@"`
@@ -52,9 +55,10 @@ else
         grep "fastq" `#only keep files with FASTQ in name (case insensitive)` | \
         grep -v "Undetermined" `#remove Undetermined FASTQs` | \
         sed -e 's/_R1.*$//g' | sed -e 's/_R2.*$//g' `#remove _R1/2_fastq.gz file extension`| \
-        sort -u > ${fastq_list} #sort and remove duplicate names, to generate list of unique FASTQs
+        sort -u  `#sort and remove duplicate names`| \
+        head -n -1 > ${fastq_list} `#remove the last line and generate a list of unique FASTQs`
     array_length=$(wc -l < ${fastq_list}) #get the number of FASTQs 
-
+    
     sbatch -o "${output_directory}/Logs/%A_%a.log" `#put into log` \
         -a "1-${array_length}" `#initiate job array equal to the number of fastq files` \
         -W `#indicates to the script not to move on until the sbatch operation is complete` \
