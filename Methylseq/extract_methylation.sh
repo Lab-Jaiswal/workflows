62ca5a176afcaf2fd3402af677fd596bcc0b0ff9
Kameron Rodrigues <kameronr@email.arizona.edu>
2022-01-03 14:52:00 -0800

    New params added to extract methylation script
    $methyl_control
    $methyl_control_fasta

--- Methylseq/extract_methylation.sh
+++ Methylseq/extract_methylation.sh
@@ -13,12 +13,14 @@ unmethyl_control_fasta=$2
 unmethyl_control=$3
 hydroxymethyl_control_fasta=$4
 hydroxymethyl_control=$5
-genome_path=$6
-phix_path=$7
-cores=$8
+methyl_control=$6
+methyl_control_fasta=$7
+genome_path=$8
+phix_path=$9
+temp_path=$10
 
 line_number=$SLURM_ARRAY_TASK_ID #get index of which file to process from $SLURM_ARRAY_TASK_ID provided by SLURM
-bam_file="$data_path/fastq/$unmethyl_control/$hydroxymethyl_control/genome_alignment/bam_files" #provide path to file containing list of fastq files
+bam_file="$data_path/fastq/$unmethyl_control/$hydroxymethyl_control/$methyl_control/genome_alignment/bam_files" #provide path to file containing list of fastq files
 bam_path="$(sed "${line_number}q; d" $bam_file)" #extract only the line number corresponding to $SLURM_ARRAY_TASK_ID
 
 #sample_prefix=$(ls $bam_path | grep bam | head -n 1 | sed -e 's/_S[0-9]*_L[0-9]*_[IR][0-9]_[0-9]*.*$//g')
@@ -36,7 +38,7 @@ temp_path=$(mktemp -d /tmp/tmp.XXXXXXXXXX)
 echo "temp_path is: " $temp_path
 mkdir $temp_path
 #copy fastq files to temp_path
-rsync -vur "$data_path/fastq/$unmethyl_control/$hydroxymethyl_control/genome_alignment/split_bams" "$temp_path/$unmethyl_control/$hydroxymethyl_control/genome_alignment/split_bams"
+rsync -vur "$data_path/fastq/$unmethyl_control/$hydroxymethyl_control/$methyl_control/genome_alignment/split_bams" "$temp_path/$unmethyl_control/$hydroxymethyl_control/genome_alignment/split_bams"
 
 echo "bams have been copied to the temporary file"
 
@@ -48,7 +50,8 @@ genome_name=$(basename $genome_path)
 echo $genomes_path
 rsync -vur "$genomes_path/$unmethyl_control_fasta" $temp_genomes_path
 rsync -vur "$genomes_path/$hydroxymethyl_control_fasta" $temp_genomes_path
-echo "Genome have been copied to the temporary file"
+rsync -vur "$genomes_path/$methyl_control_fasta" $temp_genomes_path
+echo "Genome have been copied to the temporary file directory"
 
 module load bismark/0.22.3
 module load samtools
@@ -61,9 +64,9 @@ bam_sample_path=$(echo $bam_path| sed 's/.bam$//')
 
 #####################previous extract_methylation.sh############################
 ################################################################################
-if [ ! -f "$temp_path/$unmethyl_control/$hydroxymethyl_control/genome_alignment/split_bams/${bam_sample_path}.bismark.cov.gz" ]; then 
-    cd "$temp_path/$unmethyl_control/$hydroxymethyl_control/genome_alignment/split_bams"
-        echo "$temp_path/$unmethyl_control/$hydroxymethyl_control/genome_alignment/split_bams/${bam_sample_path}.bismark.cov.gz"
+if [ ! -f "$temp_path/$unmethyl_control/$hydroxymethyl_control/$methyl_control/genome_alignment/split_bams/${bam_sample_path}.bismark.cov.gz" ]; then 
+    cd "$temp_path/$unmethyl_control/$hydroxymethyl_control/$methyl_control/genome_alignment/split_bams"
+        echo "$temp_path/$unmethyl_control/$hydroxymethyl_control/$methyl_control/genome_alignment/split_bams/${bam_sample_path}.bismark.cov.gz"
     #remove --cytosine_report
     bismark_methylation_extractor --buffer_size 20G --gzip --bedGraph --genome_folder $temp_genomes_path/$genome_name $bam_path --multicore $cores
         echo "extract_methylation complete"
