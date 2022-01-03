7a4140ed0af495c8b5e73b302ab751cdf8a80ade
Kameron Rodrigues <kameronr@email.arizona.edu>
2022-01-03 15:00:14 -0800

    New param added to join_coverage script
    methyl_control=$4

--- Methylseq/join_coverage.sh
+++ Methylseq/join_coverage.sh
@@ -10,7 +10,8 @@ data_path=$1
 seq_path="$1/fastq"
 unmethyl_control=$2
 hydroxymethyl_control=$3
-cores=$4
+methyl_control=$4
+cores=$5
 
 #temp_path=$(mktemp -d /tmp/tmp.XXXXXXXXXX)
 #echo "temp_path is: " $temp_path
@@ -23,14 +24,16 @@ fastq_file="${data_path}/fastq/FASTQs" #provide path to file containing list of 
 fastq_path="$(sed "${line_number}q; d" $fastq_file)" #extract only the line number corresponding to $SLURM_ARRAY_TASK_ID
 sample_name="${fastq_path##*/}"
 echo "sample name: $sample_name"
-output_bed="${sample_name}_R1_001.trimmed.fastq.gz_unmapped_reads_1.fq.gz_unmapped_reads_1_bismark_bt2_pe.bam.bedGraph"
-output_cov="${sample_name}_R1_001.trimmed.fastq.gz_unmapped_reads_1.fq.gz_unmapped_reads_1_bismark_bt2_pe.bam.bismark.cov"
+#these file names on the next 2 lines may not work because of latest changes to add $methyl_control parameter and incorrect use of "_unmapped_reads_1.fq.gz"
+output_bed="${sample_name}_R1_001.trimmed.fastq.gz_unmapped_reads_1.fq.gz_unmapped_reads_1.fq.gz_unmapped_reads_1_bismark_bt2_pe.bam.bedGraph"
+output_cov="${sample_name}_R1_001.trimmed.fastq.gz_unmapped_reads_1.fq.gz_unmapped_reads_1.fq.gz_unmapped_reads_1_bismark_bt2_pe.bam.bismark.cov"
 
-cd "$data_path/fastq/$unmethyl_control/$hydroxymethyl_control/genome_alignment" 
+cd "$data_path/fastq/$unmethyl_control/$hydroxymethyl_control/$methyl_control/genome_alignment" 
 find split_bams -name "*${sample_name}.bedGraph.gz" | xargs -I % bash -c 'zcat % >>'"$output_bed" 
 
 find split_bams -name "*${sample_name}.bismark.cov.gz" | xargs -I % bash -c 'zcat % >>'"$output_cov"
 #rsync -vur $temp_path/ "$data_path/fastq/" 
 
-gzip ${sample_name}_R1_001.trimmed.fastq.gz_unmapped_reads_1.fq.gz_unmapped_reads_1_bismark_bt2_pe.bam.bismark.cov.gz
-gzip ${sample_name}_R1_001.trimmed.fastq.gz_unmapped_reads_1.fq.gz_unmapped_reads_1_bismark_bt2_pe.bam.bedGraph.gz 
+#these file names on the next 2 lines may not work because of latest changes to add $methyl_control parameter and incorrect use of "_unmapped_reads_1.fq.gz"
+gzip ${sample_name}_R1_001.trimmed.fastq.gz_unmapped_reads_1.fq.gz_unmapped_reads_1.fq.gz_unmapped_reads_1_bismark_bt2_pe.bam.bismark.cov.gz
+gzip ${sample_name}_R1_001.trimmed.fastq.gz_unmapped_reads_1.fq.gz_unmapped_reads_1.fq.gz_unmapped_reads_1_bismark_bt2_pe.bam.bedGraph.gz 
