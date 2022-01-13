4380aaec1b39a9c1b73209a51c1e0eeefec83e0e
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2022-01-13 12:02:50 -0800

    Update sort.sh
    Add in sorting and indexing of merged files. 
    Add in if statements to control flow.

--- ATACseq_footprinting/sort.sh
+++ ATACseq_footprinting/sort.sh
@@ -25,30 +25,56 @@ PREFIX=$FILENAME
 echo "filename: $PREFIX"
 
 cd $bam_path
-number_replicates=$(find . -name "${PREFIX}_Rep[1-9]*_treat_rep1.bam" |
-      sed "s/${PREFIX}_Rep\([0-9][0-9]*\)\_treat_rep1.bam/\1/" |
-      sort -n |
-      tail -n 1)
-reps=$(basename "${number_replicates}")
 
-echo "number of replicates for ${PREFIX}: $reps"
+if [ ! -f "$bam_path/${PREFIX}_Rep1_treat_rep1.sorted.bam" ]; then
+      number_replicates=$(find . -name "${PREFIX}_Rep[1-9]*_treat_rep1.bam" |
+            sed "s/${PREFIX}_Rep\([0-9][0-9]*\)\_treat_rep1.bam/\1/" |
+            sort -n |
+            tail -n 1)
+      reps=$(basename "${number_replicates}")
 
-replicates=$(seq $reps)
-    for i in ${replicates[@]}
-        do
-            rep="${PREFIX}_Rep${i}_treat_rep1.bam"
-            bam_split_files="${bam_path}/BAMs_rep_${PREFIX}"
-            rep_sorted="${PREFIX}_Rep${i}_treat_rep1.sorted.bam" 
-            echo $rep_sorted >> $bam_split_files
-            
-            if [ ! -f "$bam_path/$rep_sorted" ]; then
-                samtools sort $rep -o $rep_sorted
-            fi
-        done
+      echo "number of replicates for ${PREFIX}: $reps"
+
+      replicates=$(seq $reps)
+          for i in ${replicates[@]}
+              do
+                  rep="${PREFIX}_Rep${i}_treat_rep1.bam"
+                  bam_split_files="${bam_path}/BAMs_rep_${PREFIX}"
+                  rep_sorted="${PREFIX}_Rep${i}_treat_rep1.sorted.bam" 
+                  echo $rep_sorted >> $bam_split_files
+
+                  if [ ! -f "$bam_path/$rep_sorted" ]; then
+                      samtools sort $rep -o $rep_sorted
+                  fi
+              done
+      echo "sorting bams complete"
+else
+      echo "bams were already sorted"
+fi
 
 #1b. merge bam files with the same condition (for any conditions that have more than 1 replicate)
 
-samtools merge -b "${bam_path}/BAMs_rep_${PREFIX}" "${PREFIX}.merged.bam"
+if [ ! -f "$bam_path/${PREFIX}.merged.bam" ]; then
+
+      samtools merge -b "${bam_path}/BAMs_rep_${PREFIX}" "${PREFIX}.merged.bam"
+      echo "merging of sorted bams complete"
+else
+      echo "sorted bams were already merged"
+fi
 
 #1c. index all resulting files (should be 1 final sorted bam for each experimental condition)
+
+if [ ! -f "$bam_path/${PREFIX}.merged.sorted.bam" ]; then
+      samtools sort "${PREFIX}.merged.bam" -o "${PREFIX}.merged.sorted.bam"
+      echo "sorting of merged bams complete"
+else
+      echo "merged bams already sorted"
+fi     
+
+if [ ! -f "$bam_path/${PREFIX}.merged.sorted.bai" ]; then
+      samtools index "${PREFIX}.merged.sorted.bam" "${PREFIX}.merged.sorted.bai"
+      echo "indexing of sorted merged bams complete"
+else
+      echo "sorted merged bams already indexed"
+fi      
 
