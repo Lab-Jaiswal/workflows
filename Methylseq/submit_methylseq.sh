321289157e18520ef10d79edc586a75bd840d3bc
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2022-02-01 10:38:17 -0800

    Update submit_methylseq.sh
    changes:
    (1) as the extract_methylation.sh and join_coverage.sh scripts are no longer running, I removed the slurm calls
    (2) allow for the user to specify an output file instead of having the output go into the data_path
    (3) create a parameter log
    (4) allow the user to name their log file to make sorting through errors easier

--- Methylseq/submit_methylseq.sh
+++ Methylseq/submit_methylseq.sh
@@ -2,32 +2,40 @@
 ##################################################################################################################################
 #############################################---STEP 1: SET UP PARAMETERS---###################################################### 
 ##################################################################################################################################
-if [ -z $1 ] || [ -z $2 ] ; then
-    echo "Format: ./submit_methylseq.sh [data_directory] [genetic_locations_file]"
+if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]; then
+    echo "Format: ./submit_methylseq.sh [data_directory] [output_path] [genetic_locations_file]"
     echo "genetic_locations_file is a txt file"
     echo "user can use argument --force to force the code to disregard the checks preventing methylseq.sh, extract_methylation.sh & join_coverage.sh from running"
     echo "user can use argument --cores to select a number of cores different from the default (24)"
+    echo "user can use argument --log_name to specify a name for the log files"
     exit 1
 else
 
-    TEMP=`getopt -o vdm: --long force,cores  -n 'submit_methylseq.sh' -- "$@"`
+    TEMP=`getopt -o vdm: --long cores:,log_name:,force  -n 'submit_methylseq.sh' -- "$@"`
         eval set -- "$TEMP"
         
         force=false
         cores=24
+        log_name="log_"
                         
     while true; do
         case "$1" in
             -f | --force ) force=true; shift 2 ;;
             --cores ) cores="$2"; shift 2;;
+            --log_name ) log_name="$2"; shift 2;;
             -- ) shift; break ;;
             * ) break ;;
         esac
     done
 
+    if [ $log_name != "log_" ]; then
+        log_name=$(echo "${log_name}_")
+    fi
+
     echo "force: $force"
     data_path=$1
-    genetic_locations=$2
+    output_path=$2
+    genetic_locations=$3
     unmethyl_control=$(sed -n '1p' $genetic_locations)
     unmethyl_control_fasta=$(sed -n '2p' $genetic_locations)
     hydroxymethyl_control=$(sed -n '3p' $genetic_locations)
@@ -52,31 +60,40 @@ else
 ##################################################################################################################################
 #######################################---STEP 2: CREATE NECESSARY FOLDERS---#####################################################
 ##################################################################################################################################
-    
-    if [ ! -d "$data_path/fastq/$unmethyl_control" ];then
-        mkdir "$data_path/fastq/$unmethyl_control"
+   
+    if [ ! -d $output_path ]; then
+        mkdir $output_path
     fi
 
-    if [ ! -d "$data_path/fastq/$unmethyl_control/$hydroxymethyl_control" ]; then
-        mkdir "$data_path/fastq/$unmethyl_control/$hydroxymethyl_control"
+    if [ ! -d "$output_path/$unmethyl_control" ];then
+        mkdir "$output_path/$unmethyl_control"
     fi
 
-    if [ ! -d "$data_path/fastq/$unmethyl_control/$hydroxymethyl_control/$methyl_control" ]; then
-        mkdir "$data_path/fastq/$unmethyl_control/$hydroxymethyl_control/$methyl_control"
+    if [ ! -d "$output_path/$unmethyl_control/$hydroxymethyl_control" ]; then
+        mkdir "$output_path/$unmethyl_control/$hydroxymethyl_control"
     fi
 
-    if [ ! -d "$data_path/fastq/$unmethyl_control/$hydroxymethyl_control/$methyl_control/genome_alignment" ]; then
-        mkdir "$data_path/fastq/$unmethyl_control/$hydroxymethyl_control/$methyl_control/genome_alignment"
+    if [ ! -d "$output_path/$unmethyl_control/$hydroxymethyl_control/$methyl_control" ]; then
+        mkdir "$output_path/$unmethyl_control/$hydroxymethyl_control/$methyl_control"
     fi
 
-    if ! [ ! -d "$data_path/Logs" ]; then
-        mkdir -p "$data_path/Logs"
+    if [ ! -d "$output_path/$unmethyl_control/$hydroxymethyl_control/$methyl_control/genome_alignment" ]; then
+        mkdir "$output_path/$unmethyl_control/$hydroxymethyl_control/$methyl_control/genome_alignment"
+    fi
+
+    if [ ! -d "$output_path/Logs" ]; then
+        mkdir "$output_path/Logs"
+    fi
+
+    if [ ! -d "$output_path/Parameters" ]; then
+        mkdir "$output_path/Parameters"
     fi
 
 ##################################################################################################################################
 #######################################---STEP 3: CREATE PARAMETER LOG---#####################################################
 ##################################################################################################################################
-    parameter_file="${data_path}/Logs/methylSeqParameters.txt" #give a path to a file to store the paths to the fastq files in $fastq_directory
+    now=$(date +%m_%d_%H_%M)
+    parameter_file="${output_path}/Parameters/${now}_parameters.txt" #give a path to a file to store the paths to the fastq files in $fastq_directory
     
     if [ $force == true ] && [ $cores -eq 24 ]; then 
         echo "call made to execute code: ./submit_methylseq $data_path $genetic_locations --force
@@ -150,63 +167,77 @@ else
     echo "array length: $array_length
     " >> $parameter_file
    
-    picard=$(find "$data_path/fastq/$unmethyl_control/$hydroxymethyl_control/$methyl_control/genome_alignment" -type f | grep ".*\.bam_picard_insert_size_plot.pdf$" | sort -u | wc -l)
+    picard=$(find "$output_path/$unmethyl_control/$hydroxymethyl_control/$methyl_control/genome_alignment" -type f | grep ".*\.bam_picard_insert_size_plot.pdf$" | sort -u | wc -l)
     echo "picard: $picard"
 
     if [[ $picard -lt 1 ]] || [[ $force = true ]]; then
             echo "methylseq.sh running"
-            sbatch -o "${data_path}/Logs/%A_%a.log" `#put into log` \
+            sbatch -o "${output_path}/Logs/${log_name}%A_%a.log" `#put into log` \
                     -a "1-${array_length}" `#initiate job array equal to the number of fastq files` \
                    -W `#indicates to the script not to move on until the sbatch operation is complete` \
                     "${code_directory}/methylseq.sh" \
-                    $data_path $unmethyl_control_fasta $unmethyl_control $hydroxymethyl_control_fasta $hydroxymethyl_control $methyl_control_fasta $methyl_control $genome_path $phix_path $cores
+                    $data_path $output_path $unmethyl_control_fasta $unmethyl_control $hydroxymethyl_control_fasta $hydroxymethyl_control $methyl_control_fasta $methyl_control $genome_path $phix_path $cores $log_name $parameter_file
             wait
             echo "methylseq.sh complete"
         else
             echo "picard files already created, methylseq.sh skipped"
     fi
 
-##################################################################################################################################
-#######################################---STEP 6: RUN extract_methylation.sh---################################################### 
-##################################################################################################################################
-    bam_file="$data_path/fastq/$unmethyl_control/$hydroxymethyl_control/$methyl_control/genome_alignment/bam_files" 
-    find "$data_path/fastq/$unmethyl_control/$hydroxymethyl_control/$methyl_control/genome_alignment/split_bams" -type f | grep ".*\.bam$" | sort -u > "${bam_file}" #generate list of full paths to fastq files and save to the file in $fastq_list
-    number_bam=$(wc -l < "${bam_file}") #get the number of files
-    echo "number_bam: $number_bam"
-
-    bedgraph=$(find "$data_path/fastq/$unmethyl_control/$hydroxymethyl_control/$methyl_control/genome_alignment/split_bams" -type f | grep ".*\.bedGraph.gz$" | sort -u | wc -l)
-    echo "bedgraph: $bedgraph"
-
-    if [[ $bedgraph -lt 2 ]] || [[ $force = true ]]; then
-            echo "extract_methylation.sh running"
-            sbatch -o "${data_path}/Logs/%A_%a.log" `#put into log` \
-                    -a "1-${number_bam}" `#initiate job array equal to the number of fastq files` \
-                    -W `#indicates to the script not to move on until the sbatch operation is complete` \
-                    "${code_directory}/extract_methylation.sh" \
-                    $data_path $unmethyl_control_fasta $unmethyl_control $hydroxymethyl_control_fasta $hydroxymethyl_control $methyl_control_fasta $methyl_control $genome_path $phix_path $cores
-                wait
-            echo "extract_methylation.sh complete"
-            else
-                echo "split coverage files and bed graphs have already been created for the genome alignemnt, extract_methylation.sh skipped"
+#####################previously report_controls.sh################################
+################################################################################
+module load bismark
+    if [ ! -f "$output_path/$unmethyl_control/bismark_summary_report.txt" ]; then 
+        cd $output_path/$unmethyl_control
+            echo "$output_path/$unmethyl_control/bismark_summary_report.txt does not exist yet"
+        #http://felixkrueger.github.io/Bismark/Docs/
+        #bismark report options:
+        #--alignment_report FILE
+        #--dedup_report FILE
+        #--splitting_report FILE
+        #--mbias_report FILE
+        #--nucleotide_report FILE
+        
+        #https://rawgit.com/FelixKrueger/Bismark/master/Docs/Bismark_User_Guide.html
+        bismark2report
+        bismark2summary
+        #need to specify a nucleotide coverage report file in the above command!
+            echo "created report for unmethyl control"
+    else 
+            echo "bismark summary already completed for unmethyl control"
+    fi
+        
+    if [ ! -f "$output_path/$unmethyl_control/$hydroxymethyl_control/bismark_summary_report.txt" ]; then
+        cd $output_path/$unmethyl_control/$hydroxymethyl_control
+            echo "$output_path/$unmethyl_control/$hydroxymethyl_control/bismark_summary_report.txt does not exist yet"
+        bismark2report
+        bismark2summary
+        #need to specify a nucleotide coverage report file in the above command! ^
+            echo "created report for hydroxymethyl control"
+    else
+        echo "bismark summary already completed for hydroxymethyl control"
     fi
 
-##################################################################################################################################
-##########################################---STEP 7: RUN join_coverage.sh---###################################################### 
-##################################################################################################################################
-    bedgraph=$(find "$data_path/fastq/$unmethyl_control/$hydroxymethyl_control/$methyl_control/genome_alignment" -maxdepth 1 -type f  | grep ".*\.bedGraph.gz$" | sort -u | wc -l)
-    echo "bedgraph: $bedgraph"
-
-    if [[ $bedgraph -lt 1 ]] || [[ $force = true ]] ; then
-            echo "combining bed graphs and .cov files currently split by chromosome via join_coverage.sh"
-            sbatch -o "${data_path}/Logs/%A_%a.log" `#put into log` \
-                    -a "1-${array_length}" `#initiate job array equal to the number of fastq files` \
-                    -W `#indicates to the script not to move on until the sbatch operation is complete` \
-                    "${code_directory}/join_coverage.sh" \
-                    $data_path  $unmethyl_control  $hydroxymethyl_control $methyl_control  $cores $temp_path
-                wait
-            echo "join_coverage.sh complete"
-        else
-            echo "combined bed graphs and coverage files have already been created for the genome alignemnt, join_coverage.sh skipped"
+    if [ ! -f "$output_path/$unmethyl_control/$hydroxymethyl_control/$methyl_control/bismark_summary_report.txt" ]; then
+        cd $output_path/$unmethyl_control/$hydroxymethyl_control/$methyl_control
+            echo "$output_path/$unmethyl_control/$hydroxymethyl_control/$methyl_control/bismark_summary_report.txt does not exist yet"
+        bismark2report
+        bismark2summary
+        #need to specify a nucleotide coverage report file in the above command! ^
+            echo "created report for methyl control"
+    else
+        echo "bismark summary already completed for methyl control"
     fi
 
+    echo "report_controls complete for unmethyl, hydroxymethyl, and methyl control sequences"
+
+    if [ ! -f "$output_path/$unmethyl_control/$hydroxymethyl_control/$methyl_control/genome_alignment/bismark_summary_report.txt" ]; then 
+        #https://rawgit.com/FelixKrueger/Bismark/master/Docs/Bismark_User_Guide.html
+        cd $output_path/$unmethyl_control/$hydroxymethyl_control/$methyl_control/genome_alignment
+            echo "$output_path/$unmethyl_control/$hydroxymethyl_control/$methyl_control/genome_alignment/bismark_summary_report.txt does not exist yet"
+        bismark2report
+        bismark2summary
+            echo "report complete"
+    else
+        echo "bismark summary found and already created for genome alignment"
+    fi
 fi
