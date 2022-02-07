02ed9eb10aee07ede66e88b8c8d433465ebe6337
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2022-02-07 14:43:25 -0800

    Update extract_methyl.sh

--- Methylseq/extract_methyl.sh
+++ Methylseq/extract_methyl.sh
@@ -4,29 +4,29 @@
 
 #TO DO: test all the paths in this script!
 
-while getopts b:o: flag
-do
-    case "${flag}" in
-        b) bam=${OPTARG};;
-        o) output_directory=${OPTARG};;
-    esac
-done
+bismark_input=$1
+bismark_output=$2
+output_directory=$3
+genome_fasta_path=$4
+cores=$5
 
-
+echo "bismark_input=$bismark_input
+bismark_output=$bismark_output
+output_directory=$output_directory
+genome_fasta_path=$genome_fasta_path
+cores=$cores
+"
 
 module load bismark/0.22.3
 
 
-if [ ! -f "$temp_path/$unmethyl_control/${trimmed_R1_file_name}_bismark_bt2_pe_splitting_report.txt" ]; then  
-    cd $temp_path/$unmethyl_control
-        echo "$temp_path/$unmethyl_control/${trimmed_R1_file_name}_bismark_bt2_pe_splitting_report.txt does not exist yet"
-    bismark_methylation_extractor --gzip --cytosine_report --bedGraph --genome_folder "$temp_path/unmethyl_genome" $trimmed_R1_bismark_bam --multicore $cores
+if [ ! -f $bismark_output ]; then  
+        echo "$bismark_output does not exist yet"
+    bismark_methylation_extractor --gzip --cytosine_report --bedGraph --genome_folder "$genome_fasta_path" $bismark_input --multicore $cores
         echo "extract_methylation_controls complete for unmethyl control"
-    rsync -vur --exclude "main_genome" --exclude "unmethyl_genome" --exclude "hydroxymethyl_genome" --exclude "methyl_genome" $temp_path/ $seq_path
 else
     echo "methylation control extraction for the unmethyl control found and already created"
 fi
-
 
 
 
