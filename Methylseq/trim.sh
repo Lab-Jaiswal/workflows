0992788d0d306dc00de0e82d9c4eddf2becd8bf9
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2022-02-16 11:10:27 -0800

    Update trim.sh

--- Methylseq/trim.sh
+++ Methylseq/trim.sh
@@ -4,21 +4,17 @@
 #Is set to autodetect cores available and use them.
 module load bismark/0.22.3
 module load samtools
-while getopts r:R:t:T: flag
-do
-    case "${flag}" in
-        r) read1=${OPTARG};;
-        R) read2=${OPTARG};;
-        t) read1_trimmed=${OPTARG};;
-        T) read2_trimmed=${OPTARG};;
-	o) data_path=${OPTARG};;
-	p) parameter_file=${OPTARG};;
-    esac
-done
+
+read1=$1
+read2=$2
+read1_trimmed=$3
+read2_trimmed=$4
+data_path=$5
+parameter_file=$6
 
 echo "entering trim.sh script"
 echo "trim command used the following parameters:"
-echo "$0 -r $read1 -R $read2 -t $read1_trimmed -T $read2_trimmed -o $data_path"
+echo "$0 $1 $2 $3 $4 $5 $6"
 
 if [ $SLURM_ARRAY_TASK_ID -eq 1 ]; then
 	echo "command given to trim.sh includes the following parameters:
@@ -36,11 +32,11 @@ parent_directory="$(dirname "$read1_trimmed")"
 
 echo "Parent directory for trimming is '$parent_directory'"
 
-if [ ! -f "$read1_trimmed" ] && [ ! -f "$read2_trimmed" ]; then			
+if [[ ! -f "$read1_trimmed" ]]; then			
 	echo "$(basename "$read1") and $(basename "$read2") read pair not yet trimmed. Trimming now."
 	cutadapt -q 20 -m 15 -a AGATCGGAAGAGC -A AAATCAAAAAAAC -o $read1_trimmed -p $read2_trimmed $read1 $read2 -j 0
 	echo "finished trimming $(basename $read1) and $(basename $read2) read pair."
-	rsync -vur $parent_directory/ $data_path
+	rsync -vur $parent_directory/ "$data_path/fastq"
 else
 	echo "$(basename "$read1") and $(basename "$read2") have already been trimmed"
 fi
