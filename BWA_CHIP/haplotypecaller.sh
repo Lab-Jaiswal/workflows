b5657b4aebee4a5f2a60e4fd0dfd02e8f5a75940
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2022-02-24 15:57:52 -0800

    Update haplotypecaller.sh

--- BWA_CHIP/haplotypecaller.sh
+++ BWA_CHIP/haplotypecaller.sh
@@ -5,16 +5,18 @@ echo "entering haplotypecaller.sh script"
 SAMPLE_NAME=$1
 BWA_GREF=$2
 TWIST_SNPS=$3
+PARAMETER_FILE=$4
 
 echo "haplotypecaller command used the following parameters:
-$0 $1 $2 $3"
+$0 $1 $2 $3 $4"
 
 if [ $SLURM_ARRAY_TASK_ID -eq 1 ]; then
         echo "arguments used for the haplotypecaller.sh script:
             SAMPLE_NAME=$1
             BWA_GREF=$2
             TWIST_SNPS=$3
-             " >> $parameter_file
+            PARAMETER_FILE=$4
+             " >> $PARAMETER_FILE
 fi
 
 if [ ! -f "${SAMPLE_NAME}_haplotypecaller.gvcf" ]; then
