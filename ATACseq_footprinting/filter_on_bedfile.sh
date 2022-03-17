9789fca52b8cdb17179f03a1a0209c823e0bae7e
Kameron Rodrigues <kameronr@email.arizona.edu>
2022-03-17 09:55:40 -0700

    Index filtered bam

--- ATACseq_footprinting/filter_on_bedfile.sh
+++ ATACseq_footprinting/filter_on_bedfile.sh
@@ -22,11 +22,13 @@ if [ $SLURM_ARRAY_TASK_ID -eq 1 ]; then
             " >> $parameter_file
 fi
 
-###########################---STEP 1: FILTERING WITH BEDTOOLS INTERSECT---########################################
+###########################---STEP 1: FILTERING WITH BEDTOOLS INTERSECT AND SAMTOOLS INDEX---########################################
 if [ ! -f "$output_temp_dir/${PREFIX}.merged.sorted.filtered.bam" ]; then
     module load bedtools
+    module load samtools/1.9
     echo "filtering ${PREFIX}.merged.sorted.bam using $bed_file"
     bedtools intersect -abam $output_temp_dir/${PREFIX}.merged.sorted.bam -b $bed_file > $output_temp_dir/${PREFIX}.merged.sorted.filtered.bam
+    samtools index $output_temp_dir/${PREFIX}.merged.sorted.filtered.bam
     rsync -vur "$output_temp_dir/" "$output_path"
     echo "filtering of bam file complete"
 else
