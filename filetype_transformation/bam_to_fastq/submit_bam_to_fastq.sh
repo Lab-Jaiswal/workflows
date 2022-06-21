2d61f6ed9bba3c87e5da430d1b8259276a790fd7
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2022-06-21 10:01:44 -0700

    Update submit_bam_to_fastq.sh

--- filetype_transformation/bam_to_fastq/submit_bam_to_fastq.sh
+++ filetype_transformation/bam_to_fastq/submit_bam_to_fastq.sh
@@ -1,115 +1,68 @@
-	<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
-    <link rel="icon" href="/idp/favicon.ico" type="image/x-icon" />
-    <link rel="shortcut icon" href="/idp/favicon.ico" type="image/x-icon" />
-    <script src="/idp/js/jquery-1.11.1.min.js"></script>
-    <!-- bootstrap -->
-    <link type="text/css" rel="stylesheet" href="/idp/css/bootstrap.min.css" />
-    <link type="text/css" rel="stylesheet" href="/idp/css/su-identity.css" />
-    <!--[if lt IE 9]>
-    <script src="/idp/js/html5.js"></script>
-    <![endif]-->
-    <!--[if IE 8]>
-    <link rel="stylesheet" type="text/css" href="/idp/css/ie8.css" />
-    <![endif]-->
-    <!--[if IE 7]>
-    <link rel="stylesheet" type="text/css" href="/idp/css/ie7.css" />
-    <![endif]-->
-    <script src="/idp/js/login.js"></script>
-    <!-- login CSS -->
-    <link rel="stylesheet" type="text/css" href="/idp/css/login.css" />
-    <title>Stanford Login - Stale Request</title>
-  </head>
-  <body>
-    <div id="su-wrap">
-      <!-- #su-wrap start -->
-      <div id="su-content">
-        <!-- #su-content start -->
-        <!-- Brandbar snippet start -->
-        <div id="brandbar">
-          <div class="container">
-            <a href="http://www.stanford.edu"> <img src="/idp/images/brandbar-stanford-logo@2x.png" alt="Stanford University" width="153" height="22"> </a>
-          </div>
-          <!-- .container end -->
-        </div>
-        <!-- #brandbar end -->
-        <!-- Brandbar snippet end -->
+##################################################################################################################################
+#############################################---STEP 1: SET UP PARAMETERS---###################################################### 
+##################################################################################################################################
+if [ -z $1 ] || [ -z $2 ]; then
+    echo "Format: ./submit_bam_to_fastq.sh [data_directory] [output_path]"
+    exit 1
+else
+    data_directory=$1
+    output_path=$2
 
-        <div id="header">
-          <div class="container">
-            <div id="logo">
-              <img src="/idp/images/login-header@2x.png" alt="Stanford Login" width="222" height="37"> 
-              <h1 class="pagetitle">Stanford Login</h1>
-            </div>
-          </div>
-          <!-- /.container -->
-        </div>
+    code_directory=$( realpath . )
 
-        <div id="content">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-8">
+    module load samtools/1.9
 
-                <div class="info-box">
-<p>Enter the URL you want to reach in your browser's address bar and try again.</p> <br/> <p>An error occurred because you used the Back button while browsing a secure website or application, or you used a link to a web login form rather than a website.</p>                </div>
+##################################################################################################################################
+#######################################---STEP 2: CREATE NECESSARY FOLDERS---#####################################################
+##################################################################################################################################
+    if [ ! -d "$output_path/Logs" ]; then 
+            mkdir -p "$output_path/Logs"
+    fi
+            
+    if [ ! -d "$output_path/Parameters" ]; then
+            mkdir -p "$output_path/Parameters"
+    fi
 
-              </div>
+    Logs="${output_path}/Logs"
+    Parameters="${output_path}/Parameters"
 
-              <div class="col-md-4 security-info">
-                <p><strong>Important Security Information:</strong>
-Logging in lets you access other protected Stanford websites with this browser, not just the website you requested.                </p>
-                <div class="help">
-                </div>
-                <div class="settings">
-                  <p><a href="https://uit.stanford.edu/service/authentication/help" class="more-link"><span class="fa fa-chevron-circle-right"></span> <span>LOGIN HELP</span></a></p>
-                  <!-- <p><a href="/idp/idp/profile/user/prefs" class="more-link"><i class="fa fa-chevron-circle-right"></i> <span>Advanced Settings</span></a></p> -->
-                  <p><a href="https://accounts.stanford.edu/reset2step" class="more-link"><span class="fa fa-chevron-circle-right"></span> <span>TWO-STEP DEVICE UNAVAILABLE?</span></a></p>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-12">
-                <p class="fineprint">Use of this system is subject to Stanford University's rules and regulations. See the <a href="https://adminguide.stanford.edu/chapter-6" target="_blank" title="Stanford University Administrative Guide">Stanford Administrative Guide</a> for more information.</p>
-              </div>
-            </div>
-          </div>
-          <!-- .container end -->
-        </div>
-        <!-- .content end -->
+##################################################################################################################################
+#######################################---STEP 3: CREATE PARAMETER LOG---#########################################################
+##################################################################################################################################
+    now=$(date +%m_%d_%H_%M)
+    
+    parameter_file="$Parameters/${now}_parameters.txt"                                 #add date stamp to parameter files and, if provided, the log name
+    
+    touch $parameter_file
+        
+    echo "location of scripts used to run code : $code_directory
+        " > $parameter_file 
+        
+    echo "call made to execute code: $0 $1 $2
+    " >> $parameter_file
+            
+##################################################################################################################################
+##########################################---STEP 5: CONVERT BAMS TO FASTQs---#################################################### 
+##################################################################################################################################
+    cd $data_directory 
+    
+    bam_list="$data_directory/bam_files" #give a path to a file to store the paths to the fastq files in $fastq_directory
 
-      </div>
-      <!-- #su-content end -->
-    </div>
-    <!-- #su-wrap end -->
+   find -L "${data_directory}/" -type f `#list all files in ${fastq_directory}` | \
+        grep ".*\.bam$" `#only keep files with FASTQ in name (case insensitive)` | \
+        grep -v ".bai" `#remove Undetermined FASTQs` | \
+        sed -e 's/\.bam$//g' `#remove _R1/2_fastq.gz file extension` | \
+        sort -u  `#sort and remove duplicate names` > ${bam_list}
+    bam_array_length=$(wc -l < ${bam_list}) #get the number of FASTQs 
+    echo "bam array length: $bam_array_length"
+     
+    sbatch -o "$Logs/%A_%a.log" `#put into log` \
+            -a "1-${bam_array_length}" `#initiate job array equal to the number of fastq files` \
+            -W `#indicates to the script not to move on until the sbatch operation is complete` \
+            "${code_directory}/bam_to_fastq.sh" \
+            "$data_directory" \
+            "$output_path"
 
-    <!-- Global footer snippet start -->
-    <div id="global-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-6 col-sm-2" id="bottom-logo"> <a href="https://www.stanford.edu/"> <img width="105" height="49" alt="Stanford University" src="/idp/images/footer-stanford-logo@2x.png"> </a> </div>
-          <!-- #bottom-logo end -->
-          <div class="col-xs-6 col-sm-10" id="bottom-text">
-            <ul class="list-inline">
-              <li class="home"><a href="https://www.stanford.edu/">SU Home</a></li>
-              <li class="maps alt"><a data-ua-label="global-footer" data-ua-action="visit.stanford.edu/plan/maps" class="su-link" href="http://visit.stanford.edu/plan/maps.html">Maps &amp; Directions</a></li>
-              <li class="search-stanford"><a href="http://stanford.edu/search/">Search Stanford</a></li>
-              <li class="terms alt"><a href="http://www.stanford.edu/site/terms.html">Terms of Use</a></li>
-              <li class="emergency-info"><a data-ua-label="global-footer" class="su-link" href="http://emergency.stanford.edu/">Emergency Info</a></li>
-            </ul>
-          </div>
-          <!-- .bottom-text end -->
-          <p class="copyright vcard col-sm-10 col-sm-offset-2 col-xs-12">&copy; <span class="fn org">Stanford University</span>.&nbsp; <span class="adr"> <span class="locality">Stanford</span>, <span class="region">California</span> <span class="postal-code">94305</span></span>. <span id="termsofuse"><a data-ua-label="global-footer" data-ua-action="copyright-complaints" class="su-link" href="http://www.stanford.edu/group/security/dmca.html">Copyright Complaints</a>&nbsp;&nbsp;&nbsp;<a data-ua-label="global-footer" data-ua-action="trademark-notice" class="su-link" href="https://adminguide.stanford.edu/chapter-1/subchapter-5/policy-1-5-4">Trademark Notice</a></span></p>
-        </div>
-        <!-- .row end -->
-      </div>
-      <!-- .container end -->
-    </div>
+    echo "all bams have been converted to fastqs"
 
-    <!-- global-footer end -->
-  </body>
-</html>
+fi
