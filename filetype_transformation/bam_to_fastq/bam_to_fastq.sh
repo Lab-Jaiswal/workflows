dc22b8a9e387b64e7dd8983ba44ff14db7bc84fc
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2022-06-21 10:00:57 -0700

    Update bam_to_fastq.sh

--- filetype_transformation/bam_to_fastq/bam_to_fastq.sh
+++ filetype_transformation/bam_to_fastq/bam_to_fastq.sh
@@ -1,115 +1,45 @@
-	<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
-    <link rel="icon" href="/idp/favicon.ico" type="image/x-icon" />
-    <link rel="shortcut icon" href="/idp/favicon.ico" type="image/x-icon" />
-    <script src="/idp/js/jquery-1.11.1.min.js"></script>
-    <!-- bootstrap -->
-    <link type="text/css" rel="stylesheet" href="/idp/css/bootstrap.min.css" />
-    <link type="text/css" rel="stylesheet" href="/idp/css/su-identity.css" />
-    <!--[if lt IE 9]>
-    <script src="/idp/js/html5.js"></script>
-    <![endif]-->
-    <!--[if IE 8]>
-    <link rel="stylesheet" type="text/css" href="/idp/css/ie8.css" />
-    <![endif]-->
-    <!--[if IE 7]>
-    <link rel="stylesheet" type="text/css" href="/idp/css/ie7.css" />
-    <![endif]-->
-    <script src="/idp/js/login.js"></script>
-    <!-- login CSS -->
-    <link rel="stylesheet" type="text/css" href="/idp/css/login.css" />
-    <title>Stanford Login - Stale Request</title>
-  </head>
-  <body>
-    <div id="su-wrap">
-      <!-- #su-wrap start -->
-      <div id="su-content">
-        <!-- #su-content start -->
-        <!-- Brandbar snippet start -->
-        <div id="brandbar">
-          <div class="container">
-            <a href="http://www.stanford.edu"> <img src="/idp/images/brandbar-stanford-logo@2x.png" alt="Stanford University" width="153" height="22"> </a>
-          </div>
-          <!-- .container end -->
-        </div>
-        <!-- #brandbar end -->
-        <!-- Brandbar snippet end -->
+#!/bin/bash
+#SBATCH --job-name=sort
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=batch
+#SBATCH --mem=256G
+#SBATCH --time=24:00:00
+#SBATCH --account=sjaiswal
 
-        <div id="header">
-          <div class="container">
-            <div id="logo">
-              <img src="/idp/images/login-header@2x.png" alt="Stanford Login" width="222" height="37"> 
-              <h1 class="pagetitle">Stanford Login</h1>
-            </div>
-          </div>
-          <!-- /.container -->
-        </div>
+echo "entering fastq_to_bam.sh"
 
-        <div id="content">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-8">
+bam_path=$1
+output_path=$2
 
-                <div class="info-box">
-<p>Enter the URL you want to reach in your browser's address bar and try again.</p> <br/> <p>An error occurred because you used the Back button while browsing a secure website or application, or you used a link to a web login form rather than a website.</p>                </div>
+line_number=$SLURM_ARRAY_TASK_ID #get index of which file to process from $SLURM_ARRAY_TASK_ID provided by SLURM
+bam_file="${bam_path}/bam_files" #provide path to file containing list of fastq files
+bam_prefix="$(sed "${line_number}q; d" "${bam_file}")" #extract only the line number corresponding to $SLURM_ARRAY_TASK_ID
 
-              </div>
+#Collect the sample name from the BAMs file
+#Ex: If $bam_prefix is "/atac_seq/data/ATAC_tet2_KO_LDL", then the PREFIX is "ATAC_tet2_KO_LDL"
+SAMPLE_NAME=$(basename "${bam_prefix}")
+echo "SAMPLE_NAME: $SAMPLE_NAME"
 
-              <div class="col-md-4 security-info">
-                <p><strong>Important Security Information:</strong>
-Logging in lets you access other protected Stanford websites with this browser, not just the website you requested.                </p>
-                <div class="help">
-                </div>
-                <div class="settings">
-                  <p><a href="https://uit.stanford.edu/service/authentication/help" class="more-link"><span class="fa fa-chevron-circle-right"></span> <span>LOGIN HELP</span></a></p>
-                  <!-- <p><a href="/idp/idp/profile/user/prefs" class="more-link"><i class="fa fa-chevron-circle-right"></i> <span>Advanced Settings</span></a></p> -->
-                  <p><a href="https://accounts.stanford.edu/reset2step" class="more-link"><span class="fa fa-chevron-circle-right"></span> <span>TWO-STEP DEVICE UNAVAILABLE?</span></a></p>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-12">
-                <p class="fineprint">Use of this system is subject to Stanford University's rules and regulations. See the <a href="https://adminguide.stanford.edu/chapter-6" target="_blank" title="Stanford University Administrative Guide">Stanford Administrative Guide</a> for more information.</p>
-              </div>
-            </div>
-          </div>
-          <!-- .container end -->
-        </div>
-        <!-- .content end -->
+fastq_path_R1="$output_path/${SAMPLE_NAME}_R1.fq.gz"
+fastq_path_R2="$output_path/${SAMPLE_NAME}_R2.fq.gz"
 
-      </div>
-      <!-- #su-content end -->
-    </div>
-    <!-- #su-wrap end -->
+echo "bam_to_fastq command used the following parameters: 
+$0 $1 $2"
 
-    <!-- Global footer snippet start -->
-    <div id="global-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-6 col-sm-2" id="bottom-logo"> <a href="https://www.stanford.edu/"> <img width="105" height="49" alt="Stanford University" src="/idp/images/footer-stanford-logo@2x.png"> </a> </div>
-          <!-- #bottom-logo end -->
-          <div class="col-xs-6 col-sm-10" id="bottom-text">
-            <ul class="list-inline">
-              <li class="home"><a href="https://www.stanford.edu/">SU Home</a></li>
-              <li class="maps alt"><a data-ua-label="global-footer" data-ua-action="visit.stanford.edu/plan/maps" class="su-link" href="http://visit.stanford.edu/plan/maps.html">Maps &amp; Directions</a></li>
-              <li class="search-stanford"><a href="http://stanford.edu/search/">Search Stanford</a></li>
-              <li class="terms alt"><a href="http://www.stanford.edu/site/terms.html">Terms of Use</a></li>
-              <li class="emergency-info"><a data-ua-label="global-footer" class="su-link" href="http://emergency.stanford.edu/">Emergency Info</a></li>
-            </ul>
-          </div>
-          <!-- .bottom-text end -->
-          <p class="copyright vcard col-sm-10 col-sm-offset-2 col-xs-12">&copy; <span class="fn org">Stanford University</span>.&nbsp; <span class="adr"> <span class="locality">Stanford</span>, <span class="region">California</span> <span class="postal-code">94305</span></span>. <span id="termsofuse"><a data-ua-label="global-footer" data-ua-action="copyright-complaints" class="su-link" href="http://www.stanford.edu/group/security/dmca.html">Copyright Complaints</a>&nbsp;&nbsp;&nbsp;<a data-ua-label="global-footer" data-ua-action="trademark-notice" class="su-link" href="https://adminguide.stanford.edu/chapter-1/subchapter-5/policy-1-5-4">Trademark Notice</a></span></p>
-        </div>
-        <!-- .row end -->
-      </div>
-      <!-- .container end -->
-    </div>
+echo "variables used:
+SAMPLE_NAME: $SAMPLE_NAME
+R1: $fastq_path_R1
+R2: $fastq_path_R2"
 
-    <!-- global-footer end -->
-  </body>
-</html>
+if [ ! -f "$fastq_path_R1" ]; then
+    module load samtools
+    samtools collate -u -O "${SAMPLE_NAME}.bam" | \
+    samtools fastq -1 $fastq_path_R1 -2 $fastq_path_R2 -0 /dev/null -s /dev/null -n
+    echo "Conversion of bam to fastq file complete"
+else
+    echo "Conversion of bam to fastq file already completed"
+fi
+
+echo "bam_to_fastq.sh complete"
