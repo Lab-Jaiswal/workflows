0a0acbb86557c05301799ad0fbda20f434440f36
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2022-07-08 12:40:50 -0700

    Update fastq_to_bam.sh

--- BWA_CHIP/fastq_to_bam.sh
+++ BWA_CHIP/fastq_to_bam.sh
@@ -3,23 +3,23 @@
 echo "entering fastq_to_bam.sh"
 
 SAMPLE_NAME=$1
-READGROUP=$3
-BWA_GREF=$4
-R1=$5
-R2=$6
-PARAMETER_FILE=$7
+READGROUP=$2
+BWA_GREF=$3
+R1=$4
+R2=$5
+PARAMETER_FILE=$6
 
 echo "fastq_to_bam command used the following parameters:
-$0 $1 $2 $3 $4 $5 $6 $7"
+$0 $1 $2 $3 $4 $5 $6"
 
 if [ $SLURM_ARRAY_TASK_ID -eq 1 ]; then
   echo "arguments used for the fastq_to_bam.sh script:
         SAMPLE_NAME=$1
-        READGROUP=$3
-        BWA_GREF=$4
-        R1=$5
-        R2=$6
-        PARAMETER_FILE=$7
+        READGROUP=$2
+        BWA_GREF=$3
+        R1=$4
+        R2=$5
+        PARAMETER_FILE=$6
         " >> $PARAMETER_FILE
 fi
 
