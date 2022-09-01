91f2e628f147fab64d4bd2d4932ff5dc12f4f41c
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2022-09-01 11:11:53 -0700

    Update mutect_filter.sh

--- BWA_CHIP/mutect_filter.sh
+++ BWA_CHIP/mutect_filter.sh
@@ -1,115 +1,66 @@
-	<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
-    <link rel="icon" href="/idp/favicon.ico" type="image/x-icon" />
-    <link rel="shortcut icon" href="/idp/favicon.ico" type="image/x-icon" />
-    <script src="/idp/js/jquery-1.11.1.min.js"></script>
-    <!-- bootstrap -->
-    <link type="text/css" rel="stylesheet" href="/idp/css/bootstrap.min.css" />
-    <link type="text/css" rel="stylesheet" href="/idp/css/su-identity.css" />
-    <!--[if lt IE 9]>
-    <script src="/idp/js/html5.js"></script>
-    <![endif]-->
-    <!--[if IE 8]>
-    <link rel="stylesheet" type="text/css" href="/idp/css/ie8.css" />
-    <![endif]-->
-    <!--[if IE 7]>
-    <link rel="stylesheet" type="text/css" href="/idp/css/ie7.css" />
-    <![endif]-->
-    <script src="/idp/js/login.js"></script>
-    <!-- login CSS -->
-    <link rel="stylesheet" type="text/css" href="/idp/css/login.css" />
-    <title>Stanford Login - Stale Request</title>
-  </head>
-  <body>
-    <div id="su-wrap">
-      <!-- #su-wrap start -->
-      <div id="su-content">
-        <!-- #su-content start -->
-        <!-- Brandbar snippet start -->
-        <div id="brandbar">
-          <div class="container">
-            <a href="http://www.stanford.edu"> <img src="/idp/images/brandbar-stanford-logo@2x.png" alt="Stanford University" width="153" height="22"> </a>
-          </div>
-          <!-- .container end -->
-        </div>
-        <!-- #brandbar end -->
-        <!-- Brandbar snippet end -->
+#!/bin/bash
 
-        <div id="header">
-          <div class="container">
-            <div id="logo">
-              <img src="/idp/images/login-header@2x.png" alt="Stanford Login" width="222" height="37"> 
-              <h1 class="pagetitle">Stanford Login</h1>
-            </div>
-          </div>
-          <!-- /.container -->
-        </div>
+echo "entering mutect script"
 
-        <div id="content">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-8">
+SAMPLE_NAME=$1
+BWA_GREF=$2
+PARAMETER_FILE="${3}"
+OUTPUTS=${4}
+OUTPUT_DIRECTORY=${5}
+NORMAL_SAMPLE=${6}
+CALCULATE_CONTAMINATION=${7}
+NORMAL_PILEUPS=${8}
 
-                <div class="info-box">
-<p>Enter the URL you want to reach in your browser's address bar and try again.</p> <br/> <p>An error occurred because you used the Back button while browsing a secure website or application, or you used a link to a web login form rather than a website.</p>                </div>
+if [ $SLURM_ARRAY_TASK_ID -eq 1 ]; then
+         echo "arguments used for the mutect.sh script:
+               SAMPLE_NAME=$1
+               BWA_GREF=$2
+               PARAMETER_FILE=${3}
+               OUTPUTS=${4}
+               OUTPUT_DIRECTORY=${5}
+               NORMAL_SAMPLE=${6}
+               CALCULATE_CONTAMINATION=${7}
+                NORMAL_PILEUPS=${8}
+                " >> $PARAMETER_FILE
+fi
 
-              </div>
+cd $OUTPUTS
 
-              <div class="col-md-4 security-info">
-                <p><strong>Important Security Information:</strong>
-Logging in lets you access other protected Stanford websites with this browser, not just the website you requested.                </p>
-                <div class="help">
-                </div>
-                <div class="settings">
-                  <p><a href="https://uit.stanford.edu/service/authentication/help" class="more-link"><span class="fa fa-chevron-circle-right"></span> <span>LOGIN HELP</span></a></p>
-                  <!-- <p><a href="/idp/idp/profile/user/prefs" class="more-link"><i class="fa fa-chevron-circle-right"></i> <span>Advanced Settings</span></a></p> -->
-                  <p><a href="https://accounts.stanford.edu/reset2step" class="more-link"><span class="fa fa-chevron-circle-right"></span> <span>TWO-STEP DEVICE UNAVAILABLE?</span></a></p>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-12">
-                <p class="fineprint">Use of this system is subject to Stanford University's rules and regulations. See the <a href="https://adminguide.stanford.edu/chapter-6" target="_blank" title="Stanford University Administrative Guide">Stanford Administrative Guide</a> for more information.</p>
-              </div>
-            </div>
-          </div>
-          <!-- .container end -->
-        </div>
-        <!-- .content end -->
+OUTPUT_NAME="${OUTPUT_DIRECTORY}/${SAMPLE_NAME}"
 
-      </div>
-      <!-- #su-content end -->
-    </div>
-    <!-- #su-wrap end -->
+# Move these two sections to mutect_filter.sh
+if [[ $CALCULATE_CONTAMINATION != false ]] && [[ ! -f "${OUTPUT_NAME}_contamination.table" ]] ; then
+    INPUTS="${SAMPLE_NAME}_pileups.table"
 
-    <!-- Global footer snippet start -->
-    <div id="global-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-6 col-sm-2" id="bottom-logo"> <a href="https://www.stanford.edu/"> <img width="105" height="49" alt="Stanford University" src="/idp/images/footer-stanford-logo@2x.png"> </a> </div>
-          <!-- #bottom-logo end -->
-          <div class="col-xs-6 col-sm-10" id="bottom-text">
-            <ul class="list-inline">
-              <li class="home"><a href="https://www.stanford.edu/">SU Home</a></li>
-              <li class="maps alt"><a data-ua-label="global-footer" data-ua-action="visit.stanford.edu/plan/maps" class="su-link" href="http://visit.stanford.edu/plan/maps.html">Maps &amp; Directions</a></li>
-              <li class="search-stanford"><a href="http://stanford.edu/search/">Search Stanford</a></li>
-              <li class="terms alt"><a href="http://www.stanford.edu/site/terms.html">Terms of Use</a></li>
-              <li class="emergency-info"><a data-ua-label="global-footer" class="su-link" href="http://emergency.stanford.edu/">Emergency Info</a></li>
-            </ul>
-          </div>
-          <!-- .bottom-text end -->
-          <p class="copyright vcard col-sm-10 col-sm-offset-2 col-xs-12">&copy; <span class="fn org">Stanford University</span>.&nbsp; <span class="adr"> <span class="locality">Stanford</span>, <span class="region">California</span> <span class="postal-code">94305</span></span>. <span id="termsofuse"><a data-ua-label="global-footer" data-ua-action="copyright-complaints" class="su-link" href="http://www.stanford.edu/group/security/dmca.html">Copyright Complaints</a>&nbsp;&nbsp;&nbsp;<a data-ua-label="global-footer" data-ua-action="trademark-notice" class="su-link" href="https://adminguide.stanford.edu/chapter-1/subchapter-5/policy-1-5-4">Trademark Notice</a></span></p>
-        </div>
-        <!-- .row end -->
-      </div>
-      <!-- .container end -->
-    </div>
+    #if [ "${NORMAL_SAMPLE}" != "false" ]; then
+        #INPUTS="${INPUTS} -matched ${NORMAL_PILEUPS}"
+    #fi
 
-    <!-- global-footer end -->
-  </body>
-</html>
+    echo "Getting contamination rate with CalculateContamination"
+    module load gatk4
+    gatk CalculateContamination \
+        --input "${INPUTS}" \
+        --output "${SAMPLE_NAME}_contamination.table"
+elif [ -f "${OUTPUT_NAME}_contamination.table" ]; then
+    echo "Contamination rate already calculated."
+    echo "Contamination table: ${SAMPLE_NAME}_contamination.table "
+else 
+    echo "Contamination calculation not requested"
+fi
+
+if [ ! -f "${OUTPUT_NAME}_mutect2_filter.vcf" ]; then
+    echo "Filtering somatic variants with FilterMutectCalls..."
+    module load gatk4
+    gatk FilterMutectCalls \
+    --variant "${SAMPLE_NAME}_mutect2.vcf" \
+    --output "${SAMPLE_NAME}_mutect2_filter.vcf" \
+    --contamination-table ${SAMPLE_NAME}_contamination.table \
+    --reference "${BWA_GREF}"
+    #need to change so is dependent on if need contamination, CANNOT USE QOUTE TRICK
+
+    echo "...somatic variants filtered."
+else
+    echo "Mutect2 somatic variants already filtered"
+fi
+
+echo "mutect_filter.sh complete"
