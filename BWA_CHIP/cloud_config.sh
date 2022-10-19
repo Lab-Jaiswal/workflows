45d191a1e5ba77d7abe046ef1b9d7a981a2d7495
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2022-10-19 12:01:38 -0700

    Update cloud_config.sh

--- BWA_CHIP/cloud_config.sh
+++ BWA_CHIP/cloud_config.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #always mandatory
-    reference_genome="~/References/GRCh38.p12.genome.u2af1l5_mask.fa"
-    gnomad_genomes="~/References/gnomad.genomes.v3.1.2.sites.maf05.vcf.bgz"
+    reference_genome=~/References/GRCh38.p12.genome.u2af1l5_mask.fa
+    gnomad_genomes=~/References/gnomad.genomes.v3.1.2.sites.maf05.vcf.bgz
 
 #mandatory if using funcotator
-    funcotator_sources="~/References/funcotator_dataSources.v1.6.20190124s"
+    funcotator_sources=~/References/funcotator_dataSources.v1.6.20190124s
 
 #mandatory if using split_by_chr
-    sequence_dictionary="~/References/GRCh38.p12.genome.u2af1l5_mask.fa.dict"
+    sequence_dictionary=~/References/GRCh38.p12.genome.u2af1l5_mask.fa.dict
 
 #Optional
-    chr_intervals="~/References/whole_genome_intervals.interval_list"
-    intervals="~/References/CHIP_exons.interval_list"
-    transcript_list="~/References/chip_transcript_list.txt"
+    chr_intervals=~/References/whole_genome_intervals.interval_list
+    intervals=~/References/CHIP_exons.interval_list
+    transcript_list=~/References/chip_transcript_list.txt
 
     #
