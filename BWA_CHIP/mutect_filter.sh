e0744e64a9e40ee3542f975739232bf1b8245eba
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2022-10-20 12:48:44 -0700

    Update mutect_filter.sh

--- BWA_CHIP/mutect_filter.sh
+++ BWA_CHIP/mutect_filter.sh
@@ -11,7 +11,8 @@ NORMAL_SAMPLE=${6}
 NORMAL_PILEUPS=${7}
 MODE=${8}
 LINE_NUMBER=${9}
-GATK_COMMAND=${10}
+CONTAINER_ENGINE=${10}
+GATK_COMMAND=${11}
 
 if [ $LINE_NUMBER -eq 1 ]; then
          echo "arguments used for the mutect.sh script:
@@ -24,7 +25,8 @@ if [ $LINE_NUMBER -eq 1 ]; then
                NORMAL_PILEUPS=${7}
                MODE=${8}
                LINE_NUMBER=${9}
-               GATK_COMMAND=${10}
+               CONTAINER_ENGINE=${10}
+               GATK_COMMAND=${11}
                 " >> $PARAMETER_FILE
 fi
 
@@ -32,9 +34,15 @@ cd $OUTPUTS
 
 OUTPUT_NAME="${OUTPUT_DIRECTORY}/${SAMPLE_NAME}"
 
+if [[ $CONTAINER_ENGINE = "singularity" ]]; then
+         INPUT_NAME="${SAMPLE_NAME}"
+else
+         INPUT_NAME="${OUTPUT_DIRECTORY}/${SAMPLE_NAME}/${SAMPLE_NAME}"
+fi
+
 # Move these two sections to mutect_filter.sh
 if [[ ! -f "${OUTPUT_NAME}_contamination.table" ]] ; then
-    INPUTS="${SAMPLE_NAME}_pileups.table"
+    INPUTS="${INPUT_NAME}_pileups.table"
 
     # Uncomment this when testing tumor-normal
     #if [ "${NORMAL_SAMPLE}" != "false" ]; then
@@ -44,19 +52,19 @@ if [[ ! -f "${OUTPUT_NAME}_contamination.table" ]] ; then
     echo "Getting contamination rate with CalculateContamination"
     ${GATK_COMMAND} CalculateContamination \
         --input "${INPUTS}" \
-        --output "${SAMPLE_NAME}_contamination.table"
+        --output "${INPUT_NAME}_contamination.table"
 else
     echo "Contamination rate already calculated."
-    echo "Contamination table: ${SAMPLE_NAME}_contamination.table "
+    echo "Contamination table: ${INPUT_NAME}_contamination.table "
 fi
 
 if [ ! -f "${OUTPUT_NAME}_mutect2_filter.vcf" ]; then
     echo "Filtering somatic variants with FilterMutectCalls..."
     ${GATK_COMMAND} FilterMutectCalls \
-        --variant "${SAMPLE_NAME}_mutect2.vcf" \
-        --output "${SAMPLE_NAME}_mutect2_filter.vcf" \
-        --contamination-table ${SAMPLE_NAME}_contamination.table \
-        --ob-priors  ${SAMPLE_NAME}_mutect2_artifact_prior.tar.gz \
+        --variant "${INPUT_NAME}_mutect2.vcf" \
+        --output "${INPUT_NAME}_mutect2_filter.vcf" \
+        --contamination-table ${INPUT_NAME}_contamination.table \
+        --ob-priors  ${INPUT_NAME}_mutect2_artifact_prior.tar.gz \
         --reference "${BWA_GREF}"
     #need to change so is dependent on if need contamination, CANNOT USE QOUTE TRICK
 
