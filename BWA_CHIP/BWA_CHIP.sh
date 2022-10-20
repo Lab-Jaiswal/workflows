37c90b0b6a5cc02b5f7c53875bcfdc39e6197ef3
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2022-10-20 13:36:52 -0700

    Update BWA_CHIP.sh

--- BWA_CHIP/BWA_CHIP.sh
+++ BWA_CHIP/BWA_CHIP.sh
@@ -420,8 +420,16 @@ if [ $GET_MUTECT = true ]; then
       else
         echo "running LearnReadOrientationModel"
         echo "LearnReadOrientationModel input: ${OUTPUTS}/f1r2/${SAMPLE_NAME}_f1r2.tar.gz"
+        if [[ $CONTAINER_ENGINE = "singularity" ]]; then
+                F1R2_NAME=$SAMPLE_NAME
+                LROM_OUTPUTS_NAME="{OUTPUTS}/${SAMPLE_NAME}"
+        else
+                F1R2_NAME="${OUTPUT_DIRECTORY}/${SAMPLE_NAME}/f1r2/${SAMPLE_NAME}"
+                LROM_OUTPUTS_NAME="{OUTPUT_DIRECTORY}/${SAMPLE_NAME}/${SAMPLE_NAME}"
+        fi
+        
         ${gatk_command} LearnReadOrientationModel \
-            -I ${OUTPUTS}/f1r2/${SAMPLE_NAME}_f1r2.tar.gz -O "${OUTPUTS}/${SAMPLE_NAME}_mutect2_artifact_prior.tar.gz"
+            -I ${OUTPUTS}/f1r2/${F1R2_NAME}_f1r2.tar.gz -O "${LROM_OUTPUTS_NAME}_mutect2_artifact_prior.tar.gz"
        fi
     
        if [[ $MODE = "slurm" ]]; then
