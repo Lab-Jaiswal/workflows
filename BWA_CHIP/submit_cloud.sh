2fb86de1fa7345372f802c8233c29543534d7fb7
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2022-10-26 11:09:52 -0700

    Update submit_cloud.sh

--- BWA_CHIP/submit_cloud.sh
+++ BWA_CHIP/submit_cloud.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 cd ~
-mkdir filelists
+sudo apt-get update
+sudo apt-get install -y parallel
+FILELIST=~/filelists
+mkdir -p ${FILELIST}
 
-if [[ -z "$(ls -A ~/filelists)" ]]; then
-  cd filelists
-  seq 10 59 | parallel -j8 --progress --ungroup "~/workflows/BWA_CHIP/split_files.sh {}"
-  find . -type f -not -name "*.*" | xargs -I % mv % %.sh
-  sed -i '1 i\#!/bin/bash' *.sh
+if [[ -z "$(ls -A ${FILELIST})" ]]; then
+        cd ${FILELIST}
+        seq 10 59 | parallel -j8 --progress --ungroup "~/workflows/BWA_CHIP/split_files.sh {}"
+        find . -type f -not -name "*.*" | xargs -I % mv % %.sh
+        sed -i '1 i\#!/bin/bash' *.sh
 fi
 cd ~/workflows/BWA_CHIP
 
-file_list=~/filelists/meta_filelist.txt
+file_list="${FILELIST}/meta_filelist.txt"
 
-find -L "~/filelists" -type f `#list all files in ${fastq_directory}` | \
-                             grep ".*\.${sh}$" `#only keep files with FASTQ in name (case insensitive)` | \
-                             sed -e 's/\.sh$//g' | \
+find -L ${FILELIST} -type f `#list all files in ${fastq_directory}` | \
+ grep ".*\.sh$" `#only keep files with FASTQ in name (case insensitive)` | \
+                             #sed -e 's/\.sh$//g' | \
                              sort -u  `#sort and remove duplicate names` > ${file_list}
