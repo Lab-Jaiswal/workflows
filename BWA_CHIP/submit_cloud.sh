cd0ea7423a997cfe951e26f5d0ffdf65d74371af
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2022-11-21 20:20:07 -0800

    Update submit_cloud.sh

--- BWA_CHIP/submit_cloud.sh
+++ BWA_CHIP/submit_cloud.sh
@@ -2,7 +2,7 @@
 
 function main() {
 
-        array_number=$1
+        array_number=${1}
         cd ~
         File_Lists=~/file_lists
         if [ ! -p ${File_Lists} ]; then
@@ -16,24 +16,40 @@ function main() {
                 dx download -r project-G5B07V8JPkg740v9GjfF9PzV:/File_Lists/meta_filelist.txt
         fi
 
-        ARRAY_PREFIX="$(sed "${array_number}q; d" "${meta_file_list}")"
-        ARRAY_PREFIX="$(basename $ARRAY_PREFIX)"
+        for i in $(seq 1 ${array_number}); do
 
-        list_of_samples=~/file_lists/${ARRAY_PREFIX}
-        if [ ! -f ${list_of_samples} ]; then
-                cd ${File_Lists}
-                dx download -r "project-G5B07V8JPkg740v9GjfF9PzV:/File_Lists/${ARRAY_PREFIX}"
-        fi
-        cd ~
-        LOGS=~/Outputs/Logs
-        if [ ! -p $LOGS ]; then
-               mkdir -p ${LOGS}
-        fi
+                ARRAY_PREFIX="$(sed "${i}q; d" "${meta_file_list}")"
+                echo "number: $i"
+                ARRAY_PREFIX="$(basename ${ARRAY_PREFIX})"
 
-        log_file="${LOGS}/log_file_${array_number}.sh"
-        cd ~/workflows/BWA_CHIP
-        echo "submitting BWA_CHIP"
-        ./submit_BWA_CHIP.sh --working_dir ~ --file_extension cram --mutect --container_engine docker --mode cloud --array_prefix ${list_of_samples} &>${log_file}
+                list_of_samples=~/file_lists/${ARRAY_PREFIX}
+                if [ ! -f ${list_of_samples} ]; then
+                        cd ${File_Lists}
+                        dx download -r "project-G5B07V8JPkg740v9GjfF9PzV:/File_Lists/${ARRAY_PREFIX}"
+                fi
+                cd ~
+                LOGS=~/Outputs/Logs
+                if [ ! -p $LOGS ]; then
+                       mkdir -p ${LOGS}
+                fi
+
+                log_file="${LOGS}/log_file_${array_number}.sh"
+                cd ~/workflows/BWA_CHIP
+                echo "submitting BWA_CHIP"
+
+
+               docker run --rm -v /home/dnanexus:/home/dnanexus \
+               -v /usr/local/bin/dx:/usr/local/bin/dx \
+               -v /usr/local/bin/dx-jobutil-add-output:/usr/local/bin/dx-jobutil-add-output \
+               -v /usr/local/bin/:/usr/local/bin \
+               -v /usr/local/reference/:/usr/local/reference/ \
+               -w /home/dnanexus us.gcr.io/broad-gatk/gatk:4.1.6.0 \ /bin/bash /usr/local/bin/submit_BWA_CHIP.sh --working_dir ~ --file_extension cram --mutect --container_engine docker --mode cloud --array_prefix ${list_of_samples} &>${log_file}
+
+        done
+         #filtered_vcf_output=$(dx upload "${filtered_vcf[i]}" --brief)
+         #dx-jobutil-add-output filtered_vcf_output "${filtered_vcf_output}" --class=file --array
+
+./submit_BWA_CHIP.sh --working_dir ~ --file_extension cram --mutect --container_engine docker --mode cloud --array_prefix ${list_of_samples} &>${log_file}
 }
 
-#main 1
+main $1
