a879271f2a8b23d79ca939eca60df0e10ac42302
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2023-01-19 13:23:44 -0800

    Update seperate_1000_youngest.sh

--- BWA_CHIP/get_UKBB_panel_of_normals/seperate_1000_youngest.sh
+++ BWA_CHIP/get_UKBB_panel_of_normals/seperate_1000_youngest.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-#Download the 1000 youngest file list using
+#Download the 1000 youngest file list (created in get_1000_youngest_with_crams_no_predispositions.R)
 dx download project-G5B07V8JPkg740v9GjfF9PzV:/Bulk/Exome\ sequences/Exome\ OQFE\ CRAM\ files/top_1000_without_predis_with_crams
 
-#Get a list of all of the file paths using
+#Get a list of all of the WES file paths in DNANexus
+#Add a / before every space in the filepath
 dx ls --full project-G5B07V8JPkg740v9GjfF9PzV:/Bulk/Exome\ sequences/Exome\ OQFE\ CRAM\ files/ | xargs -I {} bash -c 'array=( {array[@]/#/{}/}"' > all_file_paths
 sed -e 's/\s/\ /g' < all_file_paths > all_file_paths_fixed
 
-#Filter out all filepaths to only list the 1000 youngest using:
+#Filter out all filepaths to only list the 1000 youngest
+#Add a / before every space in the filepath
+#Add the projec name to every row
 grep -f top_1000_without_predis_with_crams < all_file_paths_fixed > filepaths_of_1000_youngest
 sed -e 's/\s/\ /g' < filepaths_of_1000_youngest > filepaths_of_1000_youngest_fixed
 sed -e 's/^/project-G5B07V8JPkg740v9GjfF9PzV:/g' filepaths_of_1000_youngest_fixed > filepaths_of_1000_youngest_final
 
-#upload the 1000 youngest filepath
+#upload the 1000 youngest filepath to dna nexus
 dx upload filepaths_of_1000_youngest_final --path project-G5B07V8JPkg740v9GjfF9PzV:/Bulk/Exome\ sequences/Exome\ OQFE\ CRAM\ files/
 
 #dx download the 1000 youngest using the filepaths in step 5
